6e8e7c9009fc715ba1c9dd8f8c2e3b36cee4409d
Joanna Makarewicz <joanna.milun@gmail.com>
2023-07-31 22:32:39 +0100

    Form chaged

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,33 +1,50 @@
 import React from "react";
+import bcg11 from "../../assets/bcg11.jpeg";
 import Button from "../Button/Button";
 import "../Form/Form.scss";
 
 const Form = () => {
   return (
-    <div className="form">
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-        </div>
-        <Button buttonName={"Wyślij"} type={"submit"} />
-      </form>
+    <div className="formArea">
+    <img className="formArea__img" src={bcg11} alt="przykład ciast" />
+    <form className="form">
+      <h3 className="form__heading">Formularz kontaktowy</h3>
+
+      <label htmlFor="exampleInputEmail1"></label>
+      <input
+        type="text"
+        className="form__control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Imię i nazwisko *"
+      />
+
+      <label htmlFor="exampleInputPassword1"></label>
+      <input
+        type="email"
+        className="form__control"
+        id="exampleInputPassword1"
+        placeholder="Email *"
+      />
+
+      <label htmlFor="exampleInputPassword1"></label>
+      <input
+        type="tel"
+        className="form__control"
+        id="exampleInputPassword1"
+        placeholder="Telefon *"
+      />
+
+      <textarea
+        className="form__control form__control--message"
+        id="exampleInputPassword1"
+        placeholder="Twoja wiadomość *"
+      />
+
+      <Button buttonName={"Wyślij"} type={"submit"} />
+    </form>
     </div>
+    
   );
 };
 
