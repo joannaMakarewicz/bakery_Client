53c6fb177ff95f70de81f0bcde88e86579f65ddc
Joanna Makarewicz <joanna.milun@gmail.com>
2023-09-25 14:02:05 +0100

    Added new images & changed alt text in images

--- src/components/Main/Header/Header.tsx
+++ src/components/Main/Header/Header.tsx
@@ -3,11 +3,8 @@ import Carousel from "react-bootstrap/Carousel";
 import bcg24 from "../../../assets/bcg24.jpeg";
 import bcg17 from "../../../assets/bcg17.jpeg";
 import bcg10 from "../../../assets/bcg10.jpeg";
-import bcg21 from "../../../assets/bcg21.jpeg";
 import bcg30 from "../../../assets/bcg30.jpg";
 import bcg28 from "../../../assets/bcg28.jpg";
-import bcg3 from "../../../assets/bcg3.jpeg";
-import bcg4 from "../../../assets/bcg4.jpeg";
 import "../Header/Header.scss";
 
 const Header = () => {
@@ -17,18 +14,18 @@ const Header = () => {
         <img
           className="header__photo header__photo--1"
           src={bcg28}
-          alt="slide first"
+          alt="Słodki stół"
         />
         <Carousel className="header__photo header__photo--2">
           <Carousel.Item>
-            <img className="header__img w-100" src={bcg24} alt="First slide" />
+            <img className="header__img w-100" src={bcg24} alt="Tort biały z różowymi różami" />
           </Carousel.Item>
           <Carousel.Item>
             <div className="d-flex">
               <img
                 className="header__img w-100"
                 src={bcg10}
-                alt="Second slide"
+                alt="Tort zielony na Chrzest Święty"
               />
             </div>
           </Carousel.Item>
@@ -39,7 +36,7 @@ const Header = () => {
         <img
           className="header__photo header__photo--3"
           src={bcg30}
-          alt="slide third"
+          alt="Słodki stół"
         />
       </div>
     </header>
