96451f761fd6bc91c0fe5f5c4e4103c6e72cb65b
Joanna Makarewicz <108859510+joannaMakarewicz@users.noreply.github.com>
2023-10-10 18:02:29 +0200

    Upper picture adding (#1)
    * Adding additional photo in mainContent component
    
    * adding additional photo in mainContent in Cakes&SweetTable pages

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,43 +1,44 @@
-import React from 'react';
+import React from "react";
 import Button from "../../components/Button/Button";
+import bcg from "../../assets/weddingAdditional.jpeg";
 import "../MainContent/MainContent.scss";
 
 type MainContentProps = {
-information:{
-    topHeading:string,
-    header:string,
-    bcg?:string,
-    title?:string,
-    text?:string,
-    buttonName:string,
-    buttonLink:string
-}
+  information: {
+    topHeading: string;
+    header: string;
+    buttonName: string;
+    buttonLink: string;
+    bcg?: string;
+    title?: string;
+    text?: string;
+  };
+};
 
-
-}
-
-const MainContent = ({information}:MainContentProps) => {
-
+const MainContent = ({ information }: MainContentProps) => {
   return (
-<section className="mainContent">
-        <article className="special">
-          <p className="special__content">{information.topHeading}</p>
-          <h1 className="special__heading">{information.header}</h1>
-        </article>
-        <article className="mainContent__content">
-
-          <div className="mainContent__text">
-            <div className="mainContent__extra">
-              <span>{information.title}</span>
-            </div>
-            <p className="mainContent__description">
-            {information.text}
-            </p>
-            <Button value={information.buttonName} href={information.buttonLink} />
+    <section className="mainContent">
+      <article className="special">
+        <p className="special__content">{information.topHeading}</p>
+        <h1 className="special__heading">{information.header}</h1>
+      </article>
+      <article className="mainContent__content">
+        <div className="mainContent__content--frame">
+          <img src={information.bcg} alt="cakes" width={"100%"} />
+        </div>
+        <div className="mainContent__text">
+          <div className="mainContent__extra">
+            <span>{information.title}</span>
           </div>
-        </article>
-              </section>
-  )
-}
+          <p className="mainContent__description">{information.text}</p>
+          <Button
+            value={information.buttonName}
+            href={information.buttonLink}
+          />
+        </div>
+      </article>
+    </section>
+  );
+};
 
 export default MainContent;
