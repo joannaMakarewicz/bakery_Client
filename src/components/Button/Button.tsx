a88590a7400d88f49e4b83697bb8ee57fc3c3b56
Joanna Makarewicz <joanna.milun@gmail.com>
2023-10-11 11:26:30 +0100

    button function added ingo MainContent

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,23 @@ import { NavLink } from "react-router-dom";
 import "../Button/Button.scss";
 
 type ButtonProps = {
-    href:string;
-    value:string;
-}
+  href: string;
+  value: string;
+  setInfo?: any;
+};
 
-const Button = ({href, value}:ButtonProps) => {
-  return <div className="button">
-    <NavLink to={href} className="btn" id="button__icon">{value}</NavLink>
-  </div>;
+const Button = ({ href, value, setInfo }: ButtonProps) => {
+  return (
+    <div className="button">
+      {value === "Wycena" ? (
+        <button onClick={setInfo} className="btn" id="button__icon">{value}</button>
+      ) : (
+        <NavLink to={href} className="btn" id="button__icon">
+          {value}
+        </NavLink>
+      )}
+    </div>
+  );
 };
 
 export default Button;
